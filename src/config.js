bc635f38a2df8bc2703ef952f58890b1cea4d486
nssr251 <97800673+nssr251@users.noreply.github.com>
2026-08-22 01:18:10 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const SUPABASE_URL = "https://opuqgrrmakligzytkgnd.supabase.co/rest/v1/";
+export const SUPABASE_URL = "https://opuqgrrmakligzytkgnd.supabase.co";
 export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9wdXFncnJtYWtsaWd6eXRrZ25kIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODczMjA2MDEsImV4cCI6MjEwMjg5NjYwMX0.Tw1LQH_Fl_8kZ2JppzmtZCIOyxnkHqlv1-JsYJiFAW4";
 
 export const RESTAURANT_DEFAULTS = {
