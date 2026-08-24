1ccadcc592b9e78ad2106cb0a6c4196539689cd4
nssr251 <97800673+nssr251@users.noreply.github.com>
2026-08-24 23:19:40 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import CustomerApp from "./CustomerApp";
 import OwnerLogin from "./pages/owner/OwnerLogin";
 import OwnerHome from "./pages/owner/OwnerHome";
 import OwnerOrders from "./pages/owner/OwnerOrders";
+import OwnerMenu from "./pages/owner/OwnerMenu";
 import OwnerLayout from "./components/owner/OwnerLayout";
 import ProtectedRoute from "./components/owner/ProtectedRoute";
 
@@ -22,6 +23,7 @@ export default function App() {
         >
           <Route index element={<OwnerHome />} />
           <Route path="orders" element={<OwnerOrders />} />
+          <Route path="menu" element={<OwnerMenu />} />
         </Route>
       </Routes>
     </BrowserRouter>
