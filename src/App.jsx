896e36c93b7d800b1009b526988b1aa48fd5e374
nssr251 <97800673+nssr251@users.noreply.github.com>
2026-08-25 00:12:00 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import CustomerApp from "./CustomerApp";
+import RiderPage from "./pages/RiderPage";
 import OwnerLogin from "./pages/owner/OwnerLogin";
 import OwnerHome from "./pages/owner/OwnerHome";
 import OwnerOrders from "./pages/owner/OwnerOrders";
 import OwnerMenu from "./pages/owner/OwnerMenu";
+import OwnerRiders from "./pages/owner/OwnerRiders";
 import OwnerLayout from "./components/owner/OwnerLayout";
 import ProtectedRoute from "./components/owner/ProtectedRoute";
 
@@ -12,6 +14,7 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<CustomerApp />} />
+        <Route path="/rider/:riderId" element={<RiderPage />} />
         <Route path="/owner/login" element={<OwnerLogin />} />
         <Route
           path="/owner"
@@ -24,6 +27,7 @@ export default function App() {
           <Route index element={<OwnerHome />} />
           <Route path="orders" element={<OwnerOrders />} />
           <Route path="menu" element={<OwnerMenu />} />
+          <Route path="riders" element={<OwnerRiders />} />
         </Route>
       </Routes>
     </BrowserRouter>
